b8c17b3314437998d451cc4a942b663c22cf8213
unknown <daluna@espol.edu.ec>
2023-12-14 20:16:25 -0500

    a

---
+++ src/main/java/g3/edd_2_g3/EDD_2_G3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package g3.edd_2_g3;
+
+/**
+ *
+ * @author David
+ */
+public class EDD_2_G3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
