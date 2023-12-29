d5f0fd25831a7c1e408162dd8709e9f3fbf8279d
OwuenYagual <oayagual@espol.edu.ec>
2023-12-28 19:47:48 -0500

    cambios

--- src/main/java/EDD_2/App.java
+++ src/main/java/EDD_2/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("selecting"), 640, 480);
+        scene = new Scene(loadFXML("launcher"), 640, 480);
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/EDD_2/controllers/LauncherController.java
@@ -0,0 +1,20 @@
+package EDD_2.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author oweny
+ */
+public class LauncherController implements Initializable {
+
+   
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
